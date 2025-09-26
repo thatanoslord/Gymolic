ff612b1ce003fac0f840f624a0f4205d19d0e59a
Your thatanoslord <sabbarmoussa@gmail.com>
2025-09-26 22:07:10 +0100

    scripting burger button

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList("class", isOpen ? "ri-close-line" : "ri-mine-line")
+} );
