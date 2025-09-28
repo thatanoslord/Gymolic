8763878731715ff480d4d21642bdead1fc8dbedb
Your thatanoslord <sabbarmoussa@gmail.com>
2025-09-28 21:40:03 +0100

    ajouter des styles pour les cartes d'information dans la section "À propos"

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
-const menuBtn = document.getElementById("menu_btn");   
+const menuBtn = document.getElementById("menu_btn");
 const navLinks = document.getElementById("nav_links");
-           
 
 menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.addEventListener("class", isOpen ? "ri-close-line" : "ri-mine-line")
+  menuBtnIcon.addEventListener(
+    "class",
+    isOpen ? "ri-close-line" : "ri-mine-line"
+  );
 });
 
 navLinks.addEventListener("click", (e) => {
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-})
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
 //scrollReveal
 
 const scrollRevealOption = {
-  distance : "50",
-  origin : "bottom",
-  duration : 1000,
-}
+  distance: "50",
+  origin: "bottom",
+  duration: 1000,
+};
 
 //Header container
 
@@ -28,11 +30,31 @@ scrollReveal().reveal(".header_image img", {
   ...scrollRevealOption,
 });
 
-scrollReveal().reveal(".header_content h4, .header_content .section_header",{
+scrollReveal().reveal(".header_content h4, .header_content .section_header", {
   ...scrollRevealOption,
-  delay : 1000,
+  delay: 1000,
 });
-scrollReveal().reveal(".header_content h4, .header_content .section_header",{
+scrollReveal().reveal(".header_content h4, .header_content .section_header", {
   ...scrollRevealOption,
-  delay : 1500,
+  delay: 1500,
+});
+
+// About container
+
+scrollRevealOption().reveal("about_image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+scrollRevealOption().reveal("about_content .section_header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+scrollRevealOption().reveal("about_content .section_description", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+scrollRevealOption().reveal("about_card", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
 });
