7c672a856873e79aca9bbcb7ff89b0d7b02f05e4
Your thatanoslord <sabbarmoussa@gmail.com>
2025-09-28 20:40:16 +0100

    ajouter la fonctionnalité de révélation de défilement pour l'image d'en-tête

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,18 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 })
+
+//scrollReveal
+
+const scrollRevealOption = {
+  distance : "50",
+  origin : "bottom",
+  duration : 1000,
+}
+
+//Header container
+
+scrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+});
+
