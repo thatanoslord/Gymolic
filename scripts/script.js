b3cc5f4f4c15f75728b43e67afd4ba63d11e6d67
Your thatanoslord <sabbarmoussa@gmail.com>
2025-10-14 23:49:44 +0100

    adding new script email address func

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,9 @@ document.querySelector('.cta').addEventListener('mouseenter', () => {
 document.querySelector('.cta').addEventListener('mouseleave', () => {
   document.querySelector('.arrow').style.transform = 'translateX(0)';
 });
+
+//email address functionality
+document.querySelector('.copy-btn').addEventListener('click', () => {
+  navigator.clipboard.writeText('sabbarmoussa@gmail.com');
+  alert('Email copied to clipboard!');
+});
